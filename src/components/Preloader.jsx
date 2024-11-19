27d39af5be7d5318fe38134434f3c3056994a83b
EmiiSem <just_dragon29@mail.ru>
2024-11-19 21:14:33 +0300

    Creating a preloader on the main page

---
+++ src/components/Preloader.jsx
@@ -0,0 +1,9 @@
+function Preloader() {
+    return (
+        <div className="progress indigo accent-2">
+            <div className="indeterminate"></div>
+        </div>
+    )
+}
+
+export { Preloader }
