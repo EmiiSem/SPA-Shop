ba073373ac47f370c8409de0e76dabdb0c46ec33
EmiiSem <just_dragon29@mail.ru>
2024-11-19 21:15:34 +0300

    Creating a list of all products on the main page

---
+++ src/components/GoodsList.jsx
@@ -0,0 +1,21 @@
+import { GoodsItem } from "./GoodsItem"
+
+function GoodsList(props) {
+    const {goods = [], addToBasket = Function.prototype} = props
+
+    if(!goods.length) {
+        return <h3>Ничего не найдено</h3>
+    }
+
+    return (
+        <div className="goods">
+            {
+                goods.map((item, index) => (
+                    <GoodsItem key={item.id || index} {...item} addToBasket={addToBasket} />
+                ))
+            }
+        </div>
+    )
+}
+
+export {GoodsList}
